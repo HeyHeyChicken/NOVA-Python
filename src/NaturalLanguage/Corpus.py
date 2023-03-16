b0415eea406f851868930c16e4f6395f7ac9a4cf
Antoine Duval <antoine@cuffel.fr>
2023-03-16 21:08:25 +0100

    Update Corpus.py

--- src/NaturalLanguage/Corpus.py
+++ src/NaturalLanguage/Corpus.py
@@ -3,9 +3,9 @@ class Corpus:
             self, intent: str,
             utterances: list[str],
             answers: list[str],
-            errors: dict[str, list[str]] = {}
+            #errors: dict[str, list[str]] = {}
         ):
         self.intent: str = intent
         self.utterances: list[str] = utterances
         self.answers: list[str] = answers
-        self.errors: list[str] = errors
+        #self.errors: list[str] = errors
