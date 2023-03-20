9a673aac4e88d11526f45013a8cea4f73fa8a6c5
Antoine Duval <antoine@cuffel.fr>
2023-03-20 20:58:29 +0100

    Update index.py

--- src/plugins/led/index.py
+++ src/plugins/led/index.py
@@ -41,7 +41,7 @@ class Led:
             index += 1
 
     def __booted(self):
-        time.sleep(0.5)
+        time.sleep(0.6)
         self.booting = False
         index: int = 1
         while(index < 200):
