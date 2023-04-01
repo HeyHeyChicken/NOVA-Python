ca8c20fa1cbd769497c1980823f47ce5363b70ec
Antoine Duval <antoine@cuffel.fr>
2023-04-01 13:08:18 +0200

    Update index.py

--- src/plugins/count/index.py
+++ src/plugins/count/index.py
@@ -7,15 +7,15 @@ from src.Audio import Audio
 
 class Count:
     integers: str() = [
-        "zéro", "une", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf",
+        "zéro", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf",
         "dix", "onze", "douze", "treize", "quatorze", "quinze", "seize", "dix-sept", "dix-huit", "dix-neuf",
-        "vingt", "vingt et une", "vingt-deux", "vingt-trois", "vingt-quatre", "vingt-cinq", "vingt-six", "vingt-sept", "vingt-huit", "vingt-neuf",
-        "trente", "trente et une", "trente-deux", "trente-trois", "trente-quatre", "trente-cinq", "trente-six", "trente-sept", "trente-huit", "trente-neuf",
-        "quarante", "quarante et une", "quarante-deux", "quarante-trois", "quarante-quatre", "quarante-cinq", "quarante-six", "quarante-sept", "quarante-huit", "quarante-neuf",
-        "cinquante", "cinquante et une", "cinquante-deux", "cinquante-trois", "cinquante-quatre", "cinquante-cinq", "cinquante-six", "cinquante-sept", "cinquante-huit", "cinquante-neuf",
-        "soixante", "soixante et une", "soixante-deux", "soixante-trois", "soixante-quatre", "soixante-cinq", "soixante-six", "soixante-sept", "soixante-huit", "soixante-neuf",
+        "vingt", "vingt et un", "vingt-deux", "vingt-trois", "vingt-quatre", "vingt-cinq", "vingt-six", "vingt-sept", "vingt-huit", "vingt-neuf",
+        "trente", "trente et un", "trente-deux", "trente-trois", "trente-quatre", "trente-cinq", "trente-six", "trente-sept", "trente-huit", "trente-neuf",
+        "quarante", "quarante et un", "quarante-deux", "quarante-trois", "quarante-quatre", "quarante-cinq", "quarante-six", "quarante-sept", "quarante-huit", "quarante-neuf",
+        "cinquante", "cinquante et un", "cinquante-deux", "cinquante-trois", "cinquante-quatre", "cinquante-cinq", "cinquante-six", "cinquante-sept", "cinquante-huit", "cinquante-neuf",
+        "soixante", "soixante et un", "soixante-deux", "soixante-trois", "soixante-quatre", "soixante-cinq", "soixante-six", "soixante-sept", "soixante-huit", "soixante-neuf",
         "soixante-dix", "soixante et onze", "soixante-douze", "soixante-treize", "soixante-quatorze", "soixante-quinze", "soixante-seize", "soixante-dix-sept", "soixante-dix-huit", "soixante-dix-neuf",
-        "quatre-vingts", "quatre-vingt-une", "quatre-vingt-deux", "quatre-vingt-trois", "quatre-vingt-quatre", "quatre-vingt-cinq", "quatre-vingt-six", "quatre-vingt-sept", "quatre-vingt-huit", "quatre-vingt-neuf",
+        "quatre-vingts", "quatre-vingt-un", "quatre-vingt-deux", "quatre-vingt-trois", "quatre-vingt-quatre", "quatre-vingt-cinq", "quatre-vingt-six", "quatre-vingt-sept", "quatre-vingt-huit", "quatre-vingt-neuf",
         "quatre-vingt-dix", "quatre-vingt-onze", "quatre-vingt-douze", "quatre-vingt-treize", "quatre-vingt-quatorze", "quatre-vingt-quinze", "quatre-vingt-seize", "quatre-vingt-dix-sept", "quatre-vingt-dix-huit", "quatre-vingt-dix-neuf",
         "cent"
     ]
@@ -61,25 +61,28 @@ class Count:
             self.tts(answer)
 
     def countFromTo(self, intent: Intent, result: ProcessorResult):
-        answer: str = ""
+        fromString: int = int(intent.variables['from'])
+        toString: int = int(intent.variables['to'])
+        if fromString in self.integers:
+            if toString in self.integers:
+                fromInt: int = self.integers.index(fromString)
+                toInt: int = self.integers.index(toString)
+                answer: str = ""
 
-        inputFrom: int = int(intent.variables['from'])
-        inputTo: int = int(intent.variables['to'])
+                loopFrom: int = fromInt if fromInt < toInt else toInt
+                loopTo: int = toInt if fromInt < toInt else fromInt
+                loopTo += 1
+                
+                loopArray = range(loopFrom, loopTo)
+                loopList = list(loopArray)
+                if toInt < fromInt:
+                    loopList.reverse()
 
-        loopFrom: int = inputFrom if inputFrom < inputTo else inputTo
-        loopTo: int = inputTo if inputFrom < inputTo else inputFrom
-        loopTo += 1
-        
-        loopArray = range(loopFrom, loopTo)
-        loopList = list(loopArray)
-        if inputTo < inputFrom:
-            loopList.reverse()
-
-        for index in loopList:
-            answer += str(index)
-            if index == ((loopTo - 1) if inputFrom < inputTo else loopFrom):
-                answer += "."
-            else:
-                answer += ", "
-        self.tts(answer)
+                for index in loopList:
+                    answer += str(index)
+                    if index == ((loopTo - 1) if fromInt < toInt else loopFrom):
+                        answer += "."
+                    else:
+                        answer += ", "
+                self.tts(answer)
         
