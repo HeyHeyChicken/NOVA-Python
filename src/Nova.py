311edabc0f1c4eef227ed96b95c438c63cf6b874
Antoine Duval <antoine@cuffel.fr>
2023-04-01 13:28:09 +0200

    Update Nova.py

--- src/Nova.py
+++ src/Nova.py
@@ -44,6 +44,7 @@ class Nova:
             pluginFolderPath: str = os.path.join(pluginsDirectoryPath, pluginFolderName)
             if os.path.isdir(pluginFolderPath):
                 module = __import__("src.plugins." + pluginFolderName + ".index")
+                print(pluginFolderName)
                 plugin_class = getattr(module, "Plugin")
                 plugin_class(self.naturalLanguageProcessor, self.audio, self.TTS, self.events, self.settings)
 
