81a511bd868736706c41ef0fbf11d30cba98d7fd
Antoine Duval <33424294+HeyHeyChicken@users.noreply.github.com>
2023-04-07 15:46:46 +0200

    Update Nova.py

--- src/Nova.py
+++ src/Nova.py
@@ -99,7 +99,7 @@ class Nova:
 
         Thread(target=self.events.onBooted).start()
 
-        self.processTextFromUser("Donne-moi un chiffre entre 5 et 10")
+        self.processTextFromUser("donne-moi un chiffre entre 5 et 10")
 
         with sounddevice.RawInputStream(samplerate=self.samplerate, blocksize = self.porcupine.frame_length, device=self.device, dtype='int16', channels=1, latency='high', callback=self.callback):
             print('#' * 80)
