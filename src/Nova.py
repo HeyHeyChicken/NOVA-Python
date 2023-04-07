4640827defeb3620dffe08ec6057e3eba0cb0970
Antoine Duval <33424294+HeyHeyChicken@users.noreply.github.com>
2023-04-07 15:37:36 +0200

    Test MediaStack

--- src/Nova.py
+++ src/Nova.py
@@ -99,7 +99,7 @@ class Nova:
 
         Thread(target=self.events.onBooted).start()
 
-        self.processTextFromUser("quel est votre ip")
+        self.processTextFromUser("quelles sont les actualités")
 
         with sounddevice.RawInputStream(samplerate=self.samplerate, blocksize = self.porcupine.frame_length, device=self.device, dtype='int16', channels=1, latency='high', callback=self.callback):
             print('#' * 80)
