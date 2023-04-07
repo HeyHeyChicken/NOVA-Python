5839b8b11be866b287a669ced269e57bd4255355
Antoine Duval <33424294+HeyHeyChicken@users.noreply.github.com>
2023-04-07 16:24:59 +0200

    Update Audio.py

--- src/Audio.py
+++ src/Audio.py
@@ -1,4 +1,5 @@
-import vlc
+from vlc import MediaPlayer
+from vlc import EventType
 import sys, os
 sys.stdout = open(os.devnull, 'w')
 
@@ -10,9 +11,10 @@ class Audio:
     mixers = []
 
     def play(self, path: str):
-        player = vlc.MediaPlayer(path)
-        self.mixers.append(player)
+        player = MediaPlayer(path)
+        player.event_manager().event_attach(EventType.MediaPlayerPlaying, self.onEnd)
         print("start")
+        self.mixers.append(player)
         player.play()
         """
         pygame.init();
@@ -30,3 +32,6 @@ class Audio:
         for mixer in self.mixers:
             #mixer.music.pause()
             mixer.pause()
+
+    def onEnd(self, event):
+        print("onEnd:", event.type, event.u)
